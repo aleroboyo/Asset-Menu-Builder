669e794a1dc8ec1824bd6ecf4623bc8012f1cc65
aleroboyo <aleroboyo0@gmail.com>
2025-07-21 10:28:45 +0100

    Add Tailwind and create assetMenuBuilderAdminPage.tsx

---
+++ assetMenuBuilder/src/assetMenuBuilderAdminPage.tsx
@@ -0,0 +1,68 @@
+export function assetMenuBuilderAdminPage () {
+    return (
+        <>
+            <title>Asset Menu Builder Admin</title>
+
+            <div>
+                <div>
+                    <h1>Asset Menu Builder Admin</h1>
+                </div>
+
+                <div>
+                    <div>
+                        <div>
+                            <h2>Administration</h2>
+                        </div>
+                        <div>
+                            <h3>Create Menu Item</h3>
+                            
+                            <div>
+                                <label>Label</label>
+                                <input type="text"/>
+                            </div>
+
+                            <div>
+                                <label>Type</label>
+                                <input type="text"/>
+                            </div>
+
+                            <div>
+                                <label>Icon</label>
+                                <input type="text"/>
+                            </div>
+
+                            <div>
+                                <label>Show in Nav</label>
+                                <input type="checkbox"></input>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            <h3>Asset Menu</h3>
+                        </div>
+                        <div>
+                            <table>
+                                <thead>
+                                    <th>ID</th>
+                                    <th>Label</th>
+                                    <th>Type</th>
+                                    <th>Metadata</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>48748</td>
+                                        <td>Hp Laptop</td>
+                                        <td>Laptop</td>
+                                        <td>Show in Nav: Yes, Icon: 💻</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
